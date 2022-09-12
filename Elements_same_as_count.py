0a02bf5100f16231545a36ebfb20da78232ba5b6
Anusha-286 <84447137+Anusha-286@users.noreply.github.com>
2022-09-12 22:12:36 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] for i in l:     if i==l.count(i):         if i not in l1:             l1.append(i) if len(l1)==0:     print(-1) else:     print(*l1)

---
+++ Elements_same_as_count.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for i in l:
+    if i==l.count(i):
+        if i not in l1:
+            l1.append(i)
+if len(l1)==0:
+    print(-1)
+else:
+    print(*l1)
