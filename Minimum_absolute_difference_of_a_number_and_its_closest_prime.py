ee095224575863332da2616e66cc0a0cbca79c97
Anusha-286 <84447137+Anusha-286@users.noreply.github.com>
2022-10-11 15:19:32 +0530

    n=int(input()) nv=n while True:     nc=0     for i in range(1,nv+1):         if nv%i==0:             nc+=1     if nc==2:         break     nv+=1 pv=n while True:     pc=0     for j in range(1,pv+1):         if pv%j==0:             pc+=1     if pc==2:         break     pv-=1 a=n-pv b=nv-n print(min(a,b))

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,22 @@
+n=int(input())
+nv=n
+while True:
+    nc=0
+    for i in range(1,nv+1):
+        if nv%i==0:
+            nc+=1
+    if nc==2:
+        break
+    nv+=1
+pv=n
+while True:
+    pc=0
+    for j in range(1,pv+1):
+        if pv%j==0:
+            pc+=1
+    if pc==2:
+        break
+    pv-=1
+a=n-pv
+b=nv-n
+print(min(a,b))
